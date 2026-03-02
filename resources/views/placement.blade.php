cfec5588837fe0e2f53a7499a4e650138347e867
Crescian <lcoryed@gmail.com>
2026-03-02 15:49:47 +0800

    /dev/crescian/fixDesign

--- resources/views/placement.blade.php
+++ resources/views/placement.blade.php
@@ -435,7 +435,7 @@
             $('thead').fadeIn(200);
             $('tbody').fadeIn(200);
         });
-    }, 1000); // Optional short delay to show spinner briefly
+    }, 50); // Optional short delay to show spinner briefly
     getCompanies();
 
     function getCompanies() {
